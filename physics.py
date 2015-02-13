b7d29104bd5f01769152a43a3bdac2457988bfce
Nis Meinert <mail@nismeinert.de>
2015-02-14 00:40:23 +0100

    Planeten waren masselos.... das war zu leicht...

--- physics.py
+++ physics.py
@@ -6,8 +6,8 @@ def updateProjectileMomentum(planets, projectile, dt):
 	old = np.array(projectile.v)
 	for planet in planets:
 		dr = np.array(projectile.pos) - np.array(planet.pos)
-		f += dr / np.power(np.linalg.norm(dr), 3) 
-	
+		f += dr / np.power(np.linalg.norm(dr), 3)
+
 	new = old + f * dt
 	projectile.v = (new[0], new[1])
 	return projectile
@@ -15,14 +15,11 @@ def updateProjectileMomentum(planets, projectile, dt):
 def moveProjectile(projectile, dt):
 	old = np.array(projectile.pos)
 	v = np.array(projectile.v)
-	new = old + v * dt 
+	new = old + v * dt
 	projectile.pos = (new[0], new[1])
 	return projectile
 
 def doesProjectilePenetratePlanet(projectile, planet):
 	dr = np.linalg.norm(np.array(projectile.pos) - np.array(planet.pos))
+	return (dr < planet.rad)
 
-	if (dr < planet.rad):
-		return True
-	else:
-		return False
