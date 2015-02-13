7395221ac70d24c08c0872784f25d0adb0c15af3
Lennart Buhl <git@hackmate.de>
2015-02-14 00:56:30 +0100

    Projectile Firing

--- physics.py
+++ physics.py
@@ -1,32 +1,28 @@
 import numpy as np
 from world import Planet, Projectile
 
-class Physics:
-	def __init__(self):
-		pass
+def updateProjectileMomentum(planets, projectile, dt):
+	f = np.array([0.0, 0.0])
+	old = np.array(projectile.v)
+	for planet in planets:
+		dr = np.array(projectile.pos) - np.array(planet.pos)
+		f += dr / np.power(np.linalg.norm(dr), 3) 
 	
-	def updateProjectileMomentum(self, planets, projectile, dt):
-		f = np.array([0.0, 0.0])
-		old = np.array(projectile.v)
-		for planet in planets:
-			dr = np.array(projectile.pos) - np.array(planet.pos)
-			f += dr / np.power(np.linalg.norm(dr), 3) 
-		
-		new = old + f * dt
-		projectile.v = (new[0], new[1])
-		return projectile
-	
-	def moveProjectile(self, projectile, dt):
-		old = np.array(projectile.pos)
-		v = np.array(projectile.v)
-		new = old + v * dt 
-		projectile.pos = (new[0], new[1])
-		return projectile
-	
-	def doesProjectilePenetratePlanet(projectile, planet):
-		dr = np.linalg.norm(np.array(projectile.pos) - np.array(planet.pos))
+	new = old + f * dt
+	projectile.v = (new[0], new[1])
+	return projectile
 
-		if (dr < planet.rad):
-			return True
-		else:
-			return False
+def moveProjectile(projectile, dt):
+	old = np.array(projectile.pos)
+	v = np.array(projectile.v)
+	new = old + v * dt 
+	projectile.pos = (new[0], new[1])
+	return projectile
+
+def doesProjectilePenetratePlanet(projectile, planet):
+	dr = np.linalg.norm(np.array(projectile.pos) - np.array(planet.pos))
+
+	if (dr < planet.rad):
+		return True
+	else:
+		return False
--- world.py
+++ world.py
@@ -6,6 +6,7 @@ from numpy import sin, cos, array
 X = 1280
 Y =  720
 FPS = 30
+UNIVERSE_SPEED = 10
 
 class RoundThing(object):
 	def __init__(self, x, y, mas, rad, col):
@@ -15,7 +16,7 @@ class RoundThing(object):
 		self.color = col
 
 	def draw(self, disp):
-		pygame.draw.circle(disp, self.color, self.pos, self.radius)
+		pygame.draw.circle(disp, self.color, map(int, self.pos), self.radius)
 
 
 class Planet(RoundThing):
@@ -23,7 +24,7 @@ class Planet(RoundThing):
 		RoundThing.__init__(self, x, y, mas, rad, col)
 
 class Projectile(RoundThing):
-	def __init__(self, rx, ry, vx, vy, mas, rad, col=(255,255,255)):
+	def __init__(self, (rx, ry), (vx, vy), mas, rad, col=(255,255,255)):
 		RoundThing.__init__(self, rx, ry, mas, rad, col)
 		self.v = (vx, vy)	#direction
 
@@ -33,9 +34,11 @@ class Gunsight(object):
 		self.base = base_planet
 		self.radian = rad # 0 - 2pi
 		self.length = lenn
+		self.orientation = 0
+		self.endpoint = (0,0)
 
 	def draw(self, disp):
-		coor = self.base.pos + ( array( (cos(self.radian), sin(self.radian))) * (self.base.radius+self.length))
-		print self.base.pos, coor
-		pygame.draw.aaline(disp, self.base.color, self.base.pos, coor)
+		self.orientation = array((cos(self.radian), sin(self.radian)))
+		self.endpoint = self.base.pos + (self.orientation * (self.base.radius+self.length))
+		pygame.draw.line(disp, self.base.color, self.base.pos, self.endpoint, 2)
 
