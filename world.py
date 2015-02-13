f6441dc472549871dcfa18a6f2569b1bf28c394f
Lennart Buhl <git@hackmate.de>
2015-02-13 23:31:13 +0100

    visu prepare

--- world.py
+++ world.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 *-*
 
+
 class RoundThing(object):
 	def __init__(self, x, y, mas, rad, col):
 		self.pos = (x,y)
@@ -11,6 +12,7 @@ class RoundThing(object):
 	def draw(self, disp):
 		pygame.draw.circle(disp, self.color, self.pos, self.radius)
 
+
 class Planet(RoundThing):
 	def __init__(self, x, y, mas, rad, col=(50,50,200)):
 		RoundThing.__init__(self, x, y, mas, rad, col)
@@ -19,3 +21,13 @@ class Projectile(RoundThing):
 	def __init__(self, rx, ry, vx, vy, mas, rad, col=(255,255,255)):
 		RoundThing.__init__(self, rx, ry, mas, rad, col)
 		self.v = (vx, vy)	#direction
+
+
+class Gunsight(object):
+	def __init__(self, base_planet, direction):
+		self.base = base_planet
+		self.v = direction
+
+	def draw(self, disp):
+		pygame.draw.aaline(disp, self.base.color, self.base.pos, self.base.pos+self.v, True)
+
