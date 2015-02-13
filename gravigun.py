0d25ab42e046d62fed9987b7aefb8a42ac5da554
Lennart Buhl <git@hackmate.de>
2015-02-13 23:09:03 +0100

    starsky working

--- gravigun.py
+++ gravigun.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 *-*
 import pygame
-from pygame.locals import K_SPACE, K_ESCAPE, KEYUP, KEYDOWN, QUIT
-from random import randint, randrange
-import os.path as path
-from glob import glob
+from pygame.locals import K_LEFT, K_RIGHT, K_SPACE, K_ESCAPE, KEYUP, KEYDOWN, QUIT
+#from random import randint, randrange
+import starsky
 
 X = 1280
 Y =  720
@@ -14,32 +13,11 @@ pygame.mixer.init()
 FONT = pygame.font.Font("pixel.ttf", 48)
 TIMER = pygame.time.Clock()
 tick = 0
-KEYS = K_SPACE, K_ESCAPE
+KEYS = [K_ESCAPE, K_LEFT, K_RIGHT, K_SPACE]
 pygame.display.set_caption('Gravigun')
 DISPLAY = pygame.display.set_mode((X, Y))
 
 PLANETS = []
-
-def starsky(disp):
-	''' render astonishing star sky. '''
-	if tick % 100 == 0:
-		for _ in range(randint(0, 25 - len(starsky.stars))):
-			starsky.stars.append((randint(25, 1255), randint(-Y, 0), 0))
-	# update and delete stars
-	if tick % 3 == 0:
-		starsky.stars = [(x, y + 1, z if z == 0 or z > 190 else z + 7)
-						for x, y, z in starsky.stars if y < Y]
-	if tick % 100 == 0:
-		r = randint(0, len(starsky.stars) - 1)
-		if starsky.stars[r][2] == 0: starsky.stars[r] = (starsky.stars[r][0], starsky.stars[r][1], 1)
-
-	# render stars
-	for x, y, z in starsky.stars:
-		b = 60 + z % 190
-		pygame.draw.circle(disp, (b, b, b), (x, y), 2)
-# mode doesn't matter for the bg, so initialsing it once is ok
-starsky.stars = [(randint(25, X-25), randint(70, 650), 0) for _ in range(randint(8, 12))]
-
 
 run = True
 events = []
@@ -48,6 +26,12 @@ timeout = 0
 
 # main game loop
 while run:
+
+	DISPLAY.fill((0, 0, 0))
+
+	starsky.starsky(DISPLAY, Y, tick)
+
+
 
 	for e in pygame.event.get():
 		if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
